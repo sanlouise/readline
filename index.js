81a7beab1d892f9500cf42b8a4635ad20c03926f
sanlouise <sandra.adamshallie@gmail.com>
2017-07-24 13:24:00 -0400

    Count string length

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('Enter the first number : ', (answer1) => {
-  rl.question('Enter the second number : ', (answer2) => {
-    rl.question('Enter the third number : ', (answer3) => {
-      const sum = +answer1 + +answer2 + +answer3;
-      console.log(`The sum is ${sum}`);
-      rl.close();
-    });
-  });
+
+rl.question('What is the input text? ', (string) => {
+  const result = string.toString();
+  const toPrint = `${result} has ${result.length} characters.`
+  console.log(toPrint);
+  rl.close();
 });
+
+// Takes three inputs
+// rl.question('Enter the first number : ', (answer1) => {
+//   rl.question('Enter the second number : ', (answer2) => {
+//     rl.question('Enter the third number : ', (answer3) => {
+//       const sum = +answer1 + +answer2 + +answer3;
+//       console.log(`The sum is ${sum}`);
+//       rl.close();
+//     });
+//   });
+// });
